b367dc5999e85f296eaff297082a9667a8a5e005
Sergio-hdD <sergio-hdd@hotmail.com>
2020-05-12 01:00:26 -0300

    Estructura base, producto completo y andando, cliente vacio

---
+++ src/main/java/com/unla/Grupo14OO22020/helpers/ViewRouteHelpers.java
@@ -0,0 +1,26 @@
+package com.unla.Grupo14OO22020.helpers;
+
+public class ViewRouteHelpers {
+	public final static String INDEX = "home/index";
+	
+	public final static String CLIENT_UPDATE = "cliente/update";
+	public final static String CLIENT_ADD = "cliente/new";
+	public final static String CLIENT_INDEX = "cliente/index";
+	
+	//redirects	
+	public final static String ROUTE_INDEX = "/index";
+	public final static String CLIENT_ROOT = "/clientes";
+	public final static String EMPLEADO_ROOT="/empleados";
+	public final static String PRODUCTO_ROOT="/productos";
+
+	
+	//Empleado
+	public final static String EMPLEADO_INDEX = "empleado/index";
+	public final static String EMPLEADO_ADD = "empleado/new";
+	public final static String EMPLEADO_UPDATE = "empleado/update";
+	
+	//Producto
+	public final static String PRODUCTO_INDEX = "producto/index";
+	public final static String PRODUCTO_ADD = "producto/new";
+	public final static String PRODUCTO_UPDATE = "producto/update";
+}//Fin class
